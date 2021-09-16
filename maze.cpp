1d36f7f07562e73e863dde3037c3877f9c7f75db
Iming Cheng <cheng70124@gmail.com>
2021-09-16 21:30:48 +0800

    Grid drawing complete

--- maze.cpp
+++ maze.cpp
@@ -23,9 +23,24 @@ public:
         m_WallWidth = wallWidth;
     }
     void Start() {
-        sf::RenderWindow window(sf::VideoMode(m_GridSize.first * m_BlockSize, m_GridSize.second * m_BlockSize), "Maze");
+        int windowHeight = m_GridSize.first * (m_BlockSize + m_WallWidth) - m_WallWidth;
+        int windowWidth = m_GridSize.second * (m_BlockSize + m_WallWidth) - m_WallWidth;
 
-        std::vector<std::vector<sf::RectangleShape>> grid(m_GridSize.first, std::vector<sf::RectangleShape>(m_GridSize.second, sf::Vector2f(m_BlockSize, m_BlockSize));
+        sf::RenderWindow window(sf::VideoMode(windowHeight, windowWidth), "Maze");
+
+        std::vector<std::vector<sf::RectangleShape>> grid(
+            m_GridSize.first, std::vector<sf::RectangleShape>(
+                m_GridSize.second, sf::RectangleShape(sf::Vector2f(m_BlockSize, m_BlockSize))
+            )
+        );
+
+        for (int i = 0; i < m_GridSize.first; i++) {
+            for (int j = 0; j < m_GridSize.second; j++) {
+                grid[i][j].setFillColor(sf::Color::Blue);
+                grid[i][j].setPosition((float)i * (m_BlockSize + m_WallWidth), (float)j * (m_BlockSize + m_WallWidth));
+            }
+        }
+
 
         sf::RectangleShape rectangle(sf::Vector2f(120.f, 50.f));
         sf::CircleShape shape(100.f);
@@ -43,10 +58,12 @@ public:
             // Draw everything
             window.clear();
 
+            for (std::vector<sf::RectangleShape> column : grid) {
+                for (sf::RectangleShape block : column) {
+                    window.draw(block);
+                }
+            }
             
-            
-
-
             window.display();
         }
     }
@@ -55,7 +72,7 @@ public:
 
 int main()
 {
-    Maze maze;
+    Maze maze({5, 4}, 30, 5);
     maze.Start();
     return 0;
 }
